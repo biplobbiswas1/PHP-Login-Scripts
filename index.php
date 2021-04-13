6a9b4512cc2ad686866ce1fc713c21f412b6d7a4
Mustafijur Rahman Biplob Biswas <59193935+biplobbiswas1@users.noreply.github.com>
2021-04-13 08:14:56 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Name: Simple PHP Login & Registration Script
+ * Version: 0.0.1
+ * Author: Mustafijur Rahman Biplob Biswas
+ * Website: www.tutorialsclass.com
+ * Tutorial: https://www.facebook.com/biplob.biswas.75641
+ */
+?>
+
+<html>
+
+<head>
+
+<body>
+
+  <div><a href=" https://www.facebook.com/biplob.biswas.75641">Contact</a>
+        Welcome to Simple Login & Registration Script in PHP. This script use minimum PHP, MySQL, and HTML that is easy to understand. Try and Learn PHP login/Registration prepared by www.TutorialsClass.com
+    </div>
+    <br /> <br />
+    <div>
+        <a href="login.php"> Login</a> | <a href="register.php"> Register</a>
+    </div>
+
+</body>
+
+</html>
